ef6c6f0ca16bfbe1837a2947a2a95ebc1269c500
Pak Shing Kan <kennethkankps@gmail.com>
2021-02-07 19:36:53 -0800

    String medium

---
+++ src/String/GroupAnagrams.java
@@ -0,0 +1,25 @@
+package String;
+
+import java.util.*;
+public class GroupAnagrams {
+    public static List<List<String>> groupAnagrams(List<String> words) {
+        // Write your code here.
+        //O( w* nlogn ) time || O (wn) space
+        //w is the number of words and n is the length of the longest word
+        Map< String, List<String> > anagrams = new HashMap<>();
+
+        for(String word:words){
+            char[] wordArray = word.toCharArray();
+            Arrays.sort(wordArray);
+            String sortedWord = new String(wordArray);
+
+            if(anagrams.containsKey(sortedWord)){
+                anagrams.get(sortedWord).add(word);
+            }else{
+                anagrams.put(sortedWord,new ArrayList<String>( Arrays.asList(word)  ));
+            }
+        }
+
+        return new ArrayList(anagrams.values());
+    }
+}
